508aef2a21765f6762c96d98c5db0633a5697309
Zhaowei Zhong <zhong98zhaowei@gmail.com>
2019-01-31 19:45:36 +0800

    Initial Commit

---
+++ src/Validators/GeetestValidator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Zhaoweizhong\Geetest\Validators;
+
+
+use Zhaoweizhong\Geetest\Facades\Geetest;
+
+class GeetestValidator
+{
+
+    /**
+     * 验证规则
+     */
+    public function validate()
+    {
+        list($geetest_challenge, $geetest_validate, $geetest_seccode) = array_values(request()->only('geetest_challenge', 'geetest_validate', 'geetest_seccode'));
+        if (session()->get('gtserver') == 1) {
+            if (Geetest::successValidate($geetest_challenge, $geetest_validate, $geetest_seccode, ['user_id'=>session()->get('user_id')])) {
+                return true;
+            }
+            return false;
+        } else {
+            if (Geetest::failValidate($geetest_challenge, $geetest_validate, $geetest_seccode)) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
