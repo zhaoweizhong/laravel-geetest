b0c06045d3618ae8def989e552d8312ff030d125
Zhaowei Zhong <zhong98zhaowei@gmail.com>
2019-01-31 20:49:17 +0800

    Fix Logging 8

--- src/Validators/GeetestValidator.php
+++ src/Validators/GeetestValidator.php
@@ -16,14 +16,20 @@ class GeetestValidator
         list($geetest_challenge, $geetest_validate, $geetest_seccode) = array_values(request()->only('geetest_challenge', 'geetest_validate', 'geetest_seccode'));
         Log::info('Geetest Validate Start', ['geetest_challenge' => $geetest_challenge, 'geetest_validate' => $geetest_validate, 'geetest_seccode' => $geetest_seccode]);
         if (session()->get('gtserver') == 1) {
+            Log::info('Geetest Validate Start 1');
             if (Geetest::successValidate($geetest_challenge, $geetest_validate, $geetest_seccode, ['user_id'=>'mengtu'])) {
+                Log::info('Geetest Validate Start 2');
                 return true;
             }
+            Log::info('Geetest Validate Start 3');
             return false;
         } else {
+            Log::info('Geetest Validate Start 4');
             if (Geetest::failValidate($geetest_challenge, $geetest_validate, $geetest_seccode)) {
+                Log::info('Geetest Validate Start 5');
                 return true;
             }
+            Log::info('Geetest Validate Start 6');
             return false;
         }
     }
